b9ecb73134c8415a34de13fb75d43f0fa032ab8f
Pat Kelly <patk1002@yahoo.com>
2022-10-04 20:05:01 -0500

    Changed quoteApp.py variables from user to quote ones

--- quoteApp.py
+++ quoteApp.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 """
-quoteApp.py is the Python code to Quote-based Database Driven REST API with Python in 10 Minutes.
+quoteApp.py is the Python code to Quote-based Database-driven REST API with Python.
 Code from https://betterdatascience.com/develop-database-driven-rest-api-with-python-in-10-minutes/
 and https://gist.github.com/betterdatascience/1b552750a4ab1c6c94c1d4e451ded9f9 
 """
@@ -20,24 +20,24 @@ ma = Marshmallow(app)
 
 
 class Quote(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    quotename = db.Column(db.String(32), unique=True)
-    password = db.Column(db.String(32))
-    first_name = db.Column(db.String(32))
-    last_name = db.Column(db.String(32))
-    age = db.Column(db.Integer)
+    quote_id = db.Column(db.Integer, primary_key=True)
+    quote_nickname = db.Column(db.String(32), unique=True)
+    quote_text = db.Column(db.String(1024), unique=True)
+    quote_date = db.Column(db.String(8))
+    quote_author = db.Column(db.String(32))
+    quote_url = db.Column(db.String(64))
 
-    def __init__(self, quotename, password, first_name, last_name, age):
-        self.quotename = quotename
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age 
+    def __init__(self, quote_nickname, quote_text, quote_date, quote_author, quote_url):
+        self.quote_nickname = quote_nickname
+        self.quote_text = quote_text
+        self.quote_date = quote_date
+        self.quote_author = quote_author
+        self.quote_url = quote_url 
 
 
 class QuoteSchema(ma.Schema):
     class Meta: 
-        fields = ('id', 'quotename', 'password', 'first_name', 'last_name', 'age')
+        fields = ('quote_id', 'quote_nickname', 'quote_text', 'quote_date', 'quote_author', 'quote_url')
 
 quote_schema = QuoteSchema()
 quotes_schema = QuoteSchema(many=True)
@@ -46,68 +46,68 @@ quotes_schema = QuoteSchema(many=True)
 class QuoteManager(Resource):
     @staticmethod
     def get():
-        try: id = request.args['id']
-        except Exception as _: id = None
+        try: quote_id = request.args['quote_id']
+        except Exception as _: quote_id = None
 
-        if not id:
+        if not quote_id:
             quotes = Quote.query.all()
             return jsonify(quotes_schema.dump(quotes))
-        quote = Quote.query.get(id)
+        quote = Quote.query.get(quote_id)
         return jsonify(quote_schema.dump(quote))
 
     @staticmethod
     def post():
-        quotename = request.json['quotename']
-        password = request.json['password']
-        first_name = request.json['first_name']
-        last_name = request.json['last_name']
-        age = request.json['age']
+        quote_nickname = request.json['quote_nickname']
+        quote_text = request.json['quote_text']
+        quote_date = request.json['quote_date']
+        quote_author = request.json['quote_author']
+        quote_url = request.json['quote_url']
 
-        quote = Quote(quotename, password, first_name, last_name, age)
+        quote = Quote(quote_nickname, quote_text, quote_date, quote_author, quote_url)
         db.session.add(quote)
         db.session.commit()
         return jsonify({
-            'Message': f'Quote {first_name} {last_name} inserted.'
+            'Message': f'Quote {quote_id} inserted.'
         })
 
     @staticmethod
     def put():
-        try: id = request.args['id']
-        except Exception as _: id = None
-        if not id:
-            return jsonify({ 'Message': 'Must provide the quote ID' })
-        quote = Quote.query.get(id)
+        try: quote_id = request.args['quote_id']
+        except Exception as _: quote_id = None
+        if not quote_id:
+            return jsonify({ 'Message': 'Must provide the quote quote_id' })
+        quote = Quote.query.get(quote_id)
 
-        quotename = request.json['quotename']
-        password = request.json['password']
-        first_name = request.json['first_name']
-        last_name = request.json['last_name']
-        age = request.json['age']
+        quote_nickname = request.json['quote_nickname']
+        quote_text = request.json['quote_text']
+        quote_date = request.json['quote_date']
+        quote_author = request.json['quote_author']
+        quote_url = request.json['quote_url']
 
-        quote.quotename = quotename 
-        quote.password = password 
-        quote.first_name = first_name 
-        quote.last_name = last_name
-        quote.age = age 
+        quote.quote_nickname = quote_nickname 
+        quote.quote_text = quote_text 
+        quote.quote_date = quote_date 
+        quote.quote_author = quote_author
+        quote.quote_url = quote_url 
 
         db.session.commit()
         return jsonify({
-            'Message': f'Quote {first_name} {last_name} altered.'
+            'Message': f'Quote {quote_id} altered.'
         })
 
     @staticmethod
     def delete():
-        try: id = request.args['id']
-        except Exception as _: id = None
-        if not id:
+        try: quote_id = request.args['quote_id']
+        except Exception as _: quote_id = None
+        if not quote_id:
             return jsonify({ 'Message': 'Must provide the quote ID' })
-        quote = Quote.query.get(id)
+        quote = Quote.query.get(quote_id)
 
         db.session.delete(quote)
         db.session.commit()
 
         return jsonify({
-            'Message': f'Quote {str(id)} deleted.'
+            'Message': f'Quote {str(quote_id)} deleted.'
         })
 
 
